48e7baeed8ef3a79f97d2e142bdfb756766a0b09
JoseDanielRC <josedanielrc66@gmail.com>
2020-12-21 01:55:05 -0600

    Login

---
+++ crosleymusic/src/Login.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import fire from './Fire';
+import IniciarS from './IniciarS';
+import Register from './Register';
+const Login = (props) => {
+    const {
+        email,
+        setEmail,
+        password,
+        setPassword,
+        handleLogin,
+        handleSignup,
+        hasAccount,
+        setHasAccount,
+        emailError,
+        passwordError,
+    } = props;
+    const texto = "Iniciar Sesion";
+    const texto2="CrosleyMusic® ";
+    return (
+        <section className="login">
+            <div className="loginContainer">
+            <h1 id="titulo" style={{ 'text-align': 'center', 'color': 'white' }}>{texto2}</h1>
+                <h3 id="titulo" style={{ 'text-align': 'center', 'color': 'white' }}>{texto}</h3>
+                <label>
+                    Username
+                </label>
+                <input
+                    type="text"
+                    autoFocus required value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <p className="errorMsg">{emailError}</p>
+                <label>Password</label>
+                <input
+                    type="password"
+                    required value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <p className="errorMsg">{passwordError}</p>
+                <div className="btnContainer">
+                    {!hasAccount ? (
+                        <>
+                            <button onClick={handleLogin}>Sign in</button>
+                            <p>Don't have an account? <span onClick={() => setHasAccount(!hasAccount)}>Sign up</span></p>
+                        </>
+                    ) : (
+                            <>
+                                <button onClick={handleSignup}>Sign up</button>
+                                <p>Have an account?
+                                    <span onClick={() => setHasAccount(!hasAccount)}>Sign in</span>
+                                </p>
+                            </>
+                        )}
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Login;
